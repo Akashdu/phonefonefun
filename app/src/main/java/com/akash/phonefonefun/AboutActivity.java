7fdd360cef25b94794953858506ffcf56bbe5cdb
Akashdu <47060506+Akashdu@users.noreply.github.com>
2020-05-01 10:58:28 +0530

    Initial commit

---
+++ app/src/main/java/com/akash/phonefonefun/AboutActivity.java
@@ -0,0 +1,30 @@
+package com.akash.phonefonefun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.quaap.phonefonefun.R;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        TextView t1 = (TextView)findViewById(R.id.textView);
+        TextView t2 = (TextView)findViewById(R.id.textView2);
+
+        t1.setText(R.string.name_link);
+        t2.setText(R.string.about);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
